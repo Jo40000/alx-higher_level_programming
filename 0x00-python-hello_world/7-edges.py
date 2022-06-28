419f3053ef9adbdc47703d456bb36b8a417cf7dd
Joseph Nathan <jbaynik23@gmail.com>
2022-06-28 05:59:14 -0700

    summit

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+world_first_3 = word[:3]
+world_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 Letters: {}".format(word_first_3))
+print("last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
